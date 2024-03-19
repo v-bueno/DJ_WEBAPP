abbcaa8f34c53dbfba0cbb04fb6d46bf141c30be
BUENO Vincent <vbueno@enssat.fr>
2024-03-19 12:29:40 +0100

    Init project

---
+++ src/main/java/org/api/BookController.java
@@ -0,0 +1,15 @@
+package org.api;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/book-management")
+public class BookController {
+
+}
