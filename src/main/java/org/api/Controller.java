bfcc003e1c4776c8b3636c82778c314db1598a75
samuel <sbollot@enssat.fr>
2024-04-04 23:07:02 +0200

    Fusion avec djs

--- src/main/java/org/api/Controller.java
+++ src/main/java/org/api/Controller.java
@@ -2,16 +2,20 @@ package org.api;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
@@ -22,7 +26,7 @@ public class Controller{
 	
 private EventDAO eventDAO = new EventDAOImpl();
 private LieuDAO lieuDAO = new LieuDAOImpl();
-	
+private DJDao djDao = new DJDaoMockImpl();
 
 
 //create post and store the form received in an array list
